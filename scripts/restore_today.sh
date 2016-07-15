2a6ceee92d2635138d8d0a78132291ca41b067de
Xinjiang Shao <shaoxinjiang@gmail.com>
2016-07-15 10:32:22 -0500

    detail process for restore

--- scripts/restore_today.sh
+++ scripts/restore_today.sh
@@ -14,9 +14,11 @@ DATE=`date +%Y-%m-%d`
 SRC_ROOT="$(git rev-parse --show-toplevel)"
 
 echo "CLEAN FOLDER"
-rm -rf $SRC_ROOT/mongodb* 
+[[ -f "$SRC_ROOT/mongodb*" ]] && rm -rf "$SRC_ROOT/mongodb*"
+
 
 echo "RESTORING NOW"
 aws s3 cp s3://prephero/mongo/${DOW}/mongodb.${MONGOHOST}.${DATE}.${DOW}.tar.gz $SRC_ROOT && \
 cd $SRC_ROOT && tar -xvzf $SRC_ROOT/mongodb.${MONGOHOST}.${DATE}.${DOW}.tar.gz && \
-mongorestore --db dev --drop --objcheck $SRC_ROOT/mongodb/Backup/mongodb/mongodb.${MONGOHOST}.${DATE}.${DOW}/prephero
+mongorestore --db dev --drop --objcheck $SRC_ROOT/mongodb/Backup/mongodb/mongodb.${MONGOHOST}.${DATE}.${DOW}/prephero && \
+echo "RESTORE FINISHED"
